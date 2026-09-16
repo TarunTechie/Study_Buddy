d51a82dba3a3fc581939408a8afbb01a2cf67e38
TarunTechie <taruntechie25@gmail.com>
2026-09-16 18:27:15 +0530

    pushed requirement changes for render

--- backend/constants/models.py
+++ backend/constants/models.py
@@ -1,4 +1,6 @@
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_ollama.chat_models import ChatOllama
+from langchain_ollama.embeddings import OllamaEmbeddings
 
 GOOGLE_API_KEY = "your-google-api-key"
 
@@ -14,3 +16,7 @@ embdModel = GoogleGenerativeAIEmbeddings(
     model="models/text-embedding-004",
     google_api_key=GOOGLE_API_KEY
 )
+
+localChatModel=ChatOllama(model='gemma4', top_k=20, temperature=0.7, top_p=0.5)
+
+localEmdbModel=OllamaEmbeddings(model='embeddinggemma', num_ctx=2000)
